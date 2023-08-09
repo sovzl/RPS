ecd21f578616b7ab141eaa679b5cd6c1c4318ff9
Sovzl <soovze@gmail.com>
2023-08-09 20:20:43 +0200

    Apply styles, implement functions, edit HTML

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ buttons.forEach(button => {
         const playerChoice = button.getAttribute('data-choice')
         const computerChoice = getComputerChoice()
         const result = (playRound(playerChoice, computerChoice))
-        updateScore(result)
+        updateScore(result, playerChoice, computerChoice)
 
 
             //turn off buttons 
@@ -45,25 +45,63 @@ buttons.forEach(button => {
     })
 })
 
+const startAgainButton = document.querySelector('.reset-button');
+startAgainButton.addEventListener('click', () => {
+    // Enable buttons again
+    buttons.forEach(button => {
+        button.disabled = false;
+    });
+
+    // Reset player and computer scores
+    playerScore = 0;
+    computerScore = 0;
+
+    // Clear result display
+    // but not sure if this is correct way to do this
+    const resultDisplay = document.querySelector('.display');
+    resultDisplay.textContent = '';
+    const playerScoreDisplay = document.querySelector('.player-score');
+    const computerScoreDisplay = document.querySelector('.computer-score');
+    playerScoreDisplay.textContent = `Player Score: ${playerScore}`;
+    computerScoreDisplay.textContent = `Computer Score: ${computerScore}`;
+});
 
 
-function updateScore(result) {
-    const playerScoreDisplay = document.querySelector('.player-score')
-    const computerScoreDisplay = document.querySelector('.computer-score')
-    const resultDisplay = document.querySelector('.display')
 
-    if (result === `Player wins`) {
+function updateScore(result, playerChoice, computerChoice) {
+    const playerScoreDisplay = document.querySelector('.player-score');
+    const computerScoreDisplay = document.querySelector('.computer-score');
+    const resultDisplay = document.querySelector('.display');
+
+    if (result === 'Player wins') {
         playerScore++;
-    }   else if (result === `Computer wins`) {
+    } else if (result === 'Computer wins') {
         computerScore++;
     }
 
+    let roundResult = '';
+    if (result === 'Player wins') {
+        roundResult = 'Player wins this round!';
+    } else if (result === 'Computer wins') {
+        roundResult = 'Computer wins this round!';
+    } else {
+        roundResult = 'It\'s a draw this round.';
+    }
 
+    if (playerScore === 3) {
+        resultDisplay.textContent = `Player wins the game! Final score: Player ${playerScore} - Computer ${computerScore}`;
+    } else if (computerScore === 3) {
+        resultDisplay.textContent = `Computer wins the game! Final score: Computer ${computerScore} - Player ${playerScore}`;
+    } else {
+        resultDisplay.textContent = `${roundResult}`;
+        computerScoreDisplay.textContent = `Computer Score: ${computerScore} Computer chose: ${computerChoice}`;
+    }
 
-    playerScoreDisplay.textContent = `Player Score: ${playerScore}`
-    computerScoreDisplay.textContent = `Computer Score: ${computerScore}`
+    playerScoreDisplay.textContent = `Player Score: ${playerScore}`;
 }
 
-//add reset button 
-//add how many rounds player want to play
-//css so it wont look like shit 
+//add reset button done
+//css so it wont look like shit well kind of done xd
+//add how many rounds player want to play 
+
+
